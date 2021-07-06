9fd45efec7b126cf1d5357348b7459b9420edb9f
Younggyo Seo <seo0gyo@gmail.com>
2021-07-06 17:12:11 +0900

    Update script for published results
    use beta=0.25 for pendulum swingup for reproducing the published results

--- rad_re3/scripts/rad_re3/pendulum_swingup.sh
+++ rad_re3/scripts/rad_re3/pendulum_swingup.sh
@@ -1,4 +1,4 @@
 for seed in 12345 23451 34512 45123 51234
 do
-    python train.py env=pendulum_swingup batch_size=512 action_repeat=2 logdir=runs_rad_re3 beta_init=0.05 seed=$seed
+    python train.py env=pendulum_swingup batch_size=512 action_repeat=2 logdir=runs_rad_re3 beta_init=0.55 seed=$seed
 done
